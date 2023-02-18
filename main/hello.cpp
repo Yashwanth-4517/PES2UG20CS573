2476da9a0a8edc6a06365fe0fdc5afa280e0096f
Yashwanth-4517 <118291988+Yashwanth-4517@users.noreply.github.com>
2023-02-18 23:39:57 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, Everyone this is Yashu\n";
+  cout << "Hello, Everyone this is Yashwanth\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
